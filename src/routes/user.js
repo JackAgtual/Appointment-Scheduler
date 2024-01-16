38bc6d1f926d7d7d156a76f26ced5428b478f867
Jack Agtual <jaagtual@gmail.com>
2024-01-15 20:48:47 -0800

    Storing hashed password in db

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,25 +1,22 @@
 import { Router } from 'express'
-import { User } from '../models/user.js'
 import { UserService } from '../services/userService.js'
 
 const route = Router()
 
 route.post('/create', async (req, res) => {
-  const { email } = req.body
+  const { email, orderNumber, password } = req.body
 
-  const user = new User({
-    orderNumber: req.body.orderNumber,
-    email,
-    password: req.body.password,
-  })
-
-  if (UserService.userExists(email)) {
+  if (await UserService.userExists(email)) {
     res.status(400).json({ message: `${email} already exists.` })
     return
   }
 
   try {
-    const newUser = await user.save()
+    const newUser = await UserService.create({
+      email,
+      orderNumber,
+      plainTextPassword: password,
+    })
     res.status(201).json(newUser)
   } catch (error) {
     res.status(400).json({ message: error.message })
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,20 @@
+import bcrypt from 'bcrypt'
 import { User } from '../models/user.js'
 
 export class UserService {
   static async userExists(email) {
     const usersWithEmail = await User.find({ email })
     return usersWithEmail.length > 0
+  }
+
+  static async create({ email, orderNumber, plainTextPassword, saltRounds = 10 }) {
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hash = await bcrypt.hash(plainTextPassword, salt)
+
+    return User.create({
+      email,
+      orderNumber,
+      hashedPassword: hash,
+    })
   }
 }
