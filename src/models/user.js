43e77f39d0b9e0a5b9524919f94538cae9f6bfe6
Jack Agtual <jaagtual@gmail.com>
2024-01-18 21:48:28 -0800

    Adding endpoint to validate user

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, lowercase: true },
   orderNumber: { type: String, required: true },
   hashedPassword: { type: String, required: true },
+  salt: { type: String, required: false },
 })
 
 export const User = mongoose.model('User', userSchema)
--- src/services/userService.js
+++ src/services/userService.js
@@ -2,19 +2,39 @@ import bcrypt from 'bcrypt'
 import { User } from '../models/user.js'
 
 export class UserService {
+  static async #hashPassword(plainTextPassword, saltRounds = 10) {
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hash = await bcrypt.hash(plainTextPassword, salt)
+    return { hash, salt }
+  }
+
+  static async #validatePassword({ plainTextPassword, hashedPassword, salt }) {
+    const testHash = await bcrypt.hash(plainTextPassword, salt)
+    return testHash === hashedPassword
+  }
+
   static async userExists(email) {
     const usersWithEmail = await User.find({ email })
     return usersWithEmail.length > 0
   }
 
-  static async create({ email, orderNumber, plainTextPassword, saltRounds = 10 }) {
-    const salt = await bcrypt.genSalt(saltRounds)
-    const hash = await bcrypt.hash(plainTextPassword, salt)
+  static async create({ email, orderNumber, plainTextPassword }) {
+    const { hash, salt } = await this.#hashPassword(plainTextPassword)
 
     return User.create({
       email,
       orderNumber,
       hashedPassword: hash,
+      salt,
+    })
+  }
+
+  static async validateCredentials({ email, plainTextPassword }) {
+    const { salt, hashedPassword } = await User.findOne({ email }, 'salt hashedPassword')
+    return this.#validatePassword({
+      plainTextPassword,
+      hashedPassword,
+      salt,
     })
   }
 }
