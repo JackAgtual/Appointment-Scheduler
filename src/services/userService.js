3d10c5b536a07fcbe018fa5cd3ce8d6633f1079d
Jack Agtual <jaagtual@gmail.com>
2024-01-20 22:43:04 -0800

    Refactoring userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,24 +2,15 @@ import bcrypt from 'bcrypt'
 import { User } from '../models/user.js'
 
 export class UserService {
-  static async #hashPassword(plainTextPassword, saltRounds = 10) {
-    const salt = await bcrypt.genSalt(saltRounds)
-    const hash = await bcrypt.hash(plainTextPassword, salt)
-    return { hash, salt }
-  }
-
-  static async #validatePassword({ plainTextPassword, hashedPassword, salt }) {
-    const testHash = await bcrypt.hash(plainTextPassword, salt)
-    return testHash === hashedPassword
-  }
-
   static async userExists(email) {
     const usersWithEmail = await User.find({ email })
     return usersWithEmail.length > 0
   }
 
   static async create({ email, orderNumber, plainTextPassword }) {
-    const { hash, salt } = await this.#hashPassword(plainTextPassword)
+    const saltRounds = 10
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hash = await bcrypt.hash(plainTextPassword, salt)
 
     return User.create({
       email,
@@ -31,10 +22,7 @@ export class UserService {
 
   static async validateCredentials({ email, plainTextPassword }) {
     const { salt, hashedPassword } = await User.findOne({ email }, 'salt hashedPassword')
-    return this.#validatePassword({
-      plainTextPassword,
-      hashedPassword,
-      salt,
-    })
+    const testHash = await bcrypt.hash(plainTextPassword, salt)
+    return testHash === hashedPassword
   }
 }
