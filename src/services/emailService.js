e1855e828f00b554af3b0a7e05346f47f706046c
Jack Agtual <jaagtual@gmail.com>
2024-01-25 20:19:59 -0800

    Adding order number to email

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -8,7 +8,7 @@ const PUBLIC_KEY = process.env.EMAIL_JS_USER_ID
 const PRIVATE_KEY = process.env.EMAIL_JS_PRIVATE_KEY
 
 export class EmailService {
-  static async send({ recipient, currentDate, betterDate }) {
+  static async send({ recipient, currentDate, betterDate, orderNumber }) {
     const data = {
       service_id: SERVICE_ID,
       template_id: TEMPLATE_ID,
@@ -18,6 +18,7 @@ export class EmailService {
         recipient_email: recipient,
         current_date: currentDate.toDateString(),
         better_date: betterDate.toDateString(),
+        order_number: orderNumber,
       },
     }
 
