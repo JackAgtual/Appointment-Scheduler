e036e56af25ed247e1e60e1122269d397c28987e
Jack Agtual <jaagtual@gmail.com>
2024-02-04 20:14:31 -0800

    Removing appointments/user route

--- src/routes/appointmentsRoute.js
+++ src/routes/appointmentsRoute.js
@@ -4,35 +4,6 @@ import { UserService } from '../services/userService.js'
 
 const route = Router()
 
-route.get(
-  '/user',
-  [
-    UserService.userDoesExist,
-    UserService.validateCredentials,
-    UserService.userIsEnrolled,
-  ],
-  async (req, res) => {
-    const { email, password } = req.query
-    const orderNumber = await UserService.getOrderNumber(email)
-
-    try {
-      const { bestAppointment } = await AppointmentService.findAppointmentAndLog({
-        email,
-        password,
-        orderNumber,
-      })
-
-      if (bestAppointment === null) {
-        res.status(404).json({ message: 'Better appointment not found' })
-      } else {
-        res.json({ date: bestAppointment })
-      }
-    } catch (error) {
-      res.status(500).json({ message: error.message })
-    }
-  },
-)
-
 route.get('/all-users', async (req, res) => {
   try {
     const availableAppointments = await AppointmentService.findAppointments(
